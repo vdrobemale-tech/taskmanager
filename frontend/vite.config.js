e686576def976132b9c4842db8fe334123ad9b58
vdrobemale-tech <vdrobemale@gmail.com>
2026-07-02 09:52:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://taskmanager-r6hb.onrender.com',
     }
   }
 });
