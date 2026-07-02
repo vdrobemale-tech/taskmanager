dcf20fd2d1668321d56760e0b865fa4a07b4b326
vdrobemale-tech <vdrobemale@gmail.com>
2026-07-02 09:52:50 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://taskmanager-r6hb.onrender.com/api'
 });
 
 API.interceptors.request.use((config) => {
